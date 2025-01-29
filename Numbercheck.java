62aeaef4fe2aead680dd984c5f15497e1fce9815
Sakethganakrishna-medidi <sakethganakrishna@gmail.com>
2025-01-29 13:43:15 +0530

    Add files via upload

---
+++ Numbercheck.java
@@ -0,0 +1,14 @@
+public class Numbercheck {
+    public static void main(String[] args) {
+        int a = -15;
+        if (a > 0){
+            System.out.println("Positive");
+        }
+        if (a == 0){
+            System.out.println("Zero");
+        }
+        if (a < 0){
+            System.out.println("Negetive");
+        }
+    }
+}
